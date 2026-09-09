21e5bf987bb0628cbe3a4ac7fbe7e4e4bf12a4b7
Alex Erickson <aerick22@iastate.edu>
2026-09-09 01:52:33 -0500

    created todo_t date structure

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
-#include "imports.h"
+#include "todo.h"
+#include <time.h>
 
 int main(int argc, char** argv){
+    time_t now;
+    time(&now);
+    todo_t* new_task = todo_init(1, "Do Homework", &now, "2026-11-9");
+    if(!new_task){
+        return -1;
+    }
+    printf("%s\n", todo_get_create_date(new_task));
+    printf("%s\n", todo_get_modified_date(new_task));
+    if(todo_mark_done(new_task)){
+        return -1;
+    }
+    printf("%s\n", todo_get_modified_date(new_task));
+    todo_free(new_task);
     return 0;
 }
 
